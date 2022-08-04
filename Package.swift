04674e7736793ca94d434a4e001683ae9df9d3dd
Zanoroy <zanoroy@gmail.com>
2022-08-04 14:06:52 +0930

    Attached 2.2.3?

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.2.0"),
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.5.1"),
+        .package(url: "https://github.com/vapor/prism-fluent-kit.git", from: "1.23.5"),
+        .package(url: "https://github.com/vapor/prism-postgres-kit.git", from: "2.8.2"),
     ],
     targets: [
         .target(name: "FluentPostgresDriver", dependencies: [
